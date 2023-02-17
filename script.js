49bb64af1d9cd1a01ce20e207968cf20eeb71dcd
Baby Taz <118545043+cool0077@users.noreply.github.com>
2023-02-17 16:32:19 -0500

    Second Commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -19,7 +21,11 @@ const addItem = (e) => {
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  // Triger the function when we add any new items in the listItem
+  checkUI();
 
   itemInput.value = '';
 }
@@ -38,6 +44,55 @@ const createIcon = (classes) => {
   return icon;
 }
 
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      // Triger the function when we delete every item by 'delete-btn'
+      checkUI();
+    }
+  }
+}
+
+const clearItems = () => {
+  while(itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+const filterItems = (e) => {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+// Check if there's still any items left when we reload the page everytime
+checkUI();
